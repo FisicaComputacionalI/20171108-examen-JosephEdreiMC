6078c09e79fc6a185a18c47e1b37007957d66c96
JosephEdreiMC <joseph.edrei.mc@gmail.com>
2017-11-08 12:45:41 -0600

    Create TerceraPregunta.cc
    Tercera Pregunta del Examen.

---
+++ TerceraPregunta.cc
@@ -0,0 +1,18 @@
+//Programa que calcula la velocidad dado el tiempo
+//Nombre: Joseph Edrei Moreno Cruz
+//8 de noviembre del 2017
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+float d=0;
+float t=0;
+float v=0;
+cout<<"Ingrese el tiempo"<<endl;
+cin>>t;
+cout<<"Ingrese la distancia"<<endl;
+cin>>d;
+v=d*t;
+cout<<"La velocidad es: "<<v<<endl;
+return 0;
+}
